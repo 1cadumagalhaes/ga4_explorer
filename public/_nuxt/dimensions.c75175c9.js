ec9f96bfc0d85def331fb921204b8583f9d13fec
1cadumagalhaes <github@cadumagalhaes.dev>
2023-09-05 18:49:05 -0300

    Adding builded on public

---
+++ public/_nuxt/dimensions.c75175c9.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./Table.894f6179.js";import{_ as e,h as t,o as _}from"./entry.842af2f0.js";const c={};function r(s,a){const o=n;return _(),t(o,{filter:"dimension"})}const f=e(c,[["render",r]]);export{f as default};
---
+++ public/_nuxt/metrics.9d5301a6.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Table.894f6179.js";import{_ as t,h as c,o as r}from"./entry.842af2f0.js";const _={};function n(s,a){const e=o;return r(),c(e,{filter:"metric"})}const i=t(_,[["render",n]]);export{i as default};
