9c3b8bc8df55b9b8f317439a097979421ed33084
1cadumagalhaes <github@cadumagalhaes.dev>
2023-09-06 11:16:02 -0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: ['@nuxthq/ui', 'nuxt-icon'],
-    app: {
-        baseURL: '/ga4_explorer/',
-    },
+    // app: {
+    //     baseURL: '/ga4_explorer/',
+    // },
 });
